f42e549536ad4bb525b67d8b6dcdb1dd2ca1be44
Adrian <adrian_gecala@yahoo.com>
2025-10-20 20:43:48 +0300

    removed all modal references

--- script.js
+++ script.js
@@ -9,26 +9,6 @@ dropdownBtn.addEventListener('click', () => {
     : 'Premium Physical Robot Features ⬇';
 });
 
-// ==================== Modal Feature System ==================== //
-const tiles = document.querySelectorAll('.tile');
-const modal = document.getElementById('featureModal');
-const modalTitle = document.getElementById('modalTitle');
-const modalDescription = document.getElementById('modalDescription');
-const closeModal = document.getElementById('closeModal');
-
-
-
-// Close modal
-closeModal.addEventListener('click', () => {
-  modal.style.display = 'none';
-});
-
-// Close modal when clicking outside the box
-window.addEventListener('click', (e) => {
-  if (e.target === modal) {
-    modal.style.display = 'none';
-  }
-});
 const userType = "digital"; // "digital" or "full"
 
 const proTiles = document.querySelectorAll('#physicalGrid .tile');
